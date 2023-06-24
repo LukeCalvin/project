c22b04ce1de9fc59fed885b065fe6158359cc54f
ahakso <ahakso@bloomberg.net>
2023-06-24 16:48:23 -0700

    add custom exception

--- validate.py
+++ validate.py
@@ -1,15 +1,19 @@
 import pandas as pd
 from gspread import Worksheet
 
-HEADER_RANGE = 'A11:T11'
+HEADER_RANGE = "A11:T11"
+
+
+class SheetAssumptionViolated(Exception):
+    pass
+
 
 def validate_sheet(sheet: Worksheet) -> None:
-    pass
     # Worksheet is passed in as an arg
     # Check if the header row is where we expect it to be
-    error_msg = validate_header(sheet,HEADER_RANGE)
+    error_msg = validate_header(sheet, HEADER_RANGE)
     if error_msg is not None:
-        raise ValueError(error_msg)
+        raise SheetAssumptionViolated(error_msg)
 
     # Check if the columns we expect to exist are there
 
@@ -17,23 +21,24 @@ def validate_sheet(sheet: Worksheet) -> None:
 
     # Spot check critical columns to see if values are what we think they are
 
+
 def validate_header(sheet: Worksheet, cell_range: str) -> str | None:
     """
     :param Worksheet: a gspread worksheet object containing one circuit of tasks
     :param cell_range: the A-notation range of where the header should be
-    
+
     Return a string with missing columns, if they exist
 
     Only a subset of hard-coded expected columns; could be expanded.
     """
-    
-#     get_values returns a list of lists, which is a list of the values in each row. 
+
+    #     get_values returns a list of lists, which is a list of the values in each row.
     expected_header = sheet.get_values(cell_range)[0]
     missing_column_names = []
-    for expected_column in ['STATUS','PROJECTED HOURS','SQUIRT BOOM', 'Full Address']:
+    for expected_column in ["STATUS", "PROJECTED HOURS", "SQUIRT BOOM", "Full Address"]:
         if expected_column not in expected_header:
             print(f"{expected_column} not in the header range")
         if expected_column.lower() not in [header_val.lower() for header_val in expected_header]:
-             missing_column_names.append(expected_column)
+            missing_column_names.append(expected_column)
     if missing_column_names != []:
         print(f"{missing_column_names} not in the header range, even in constant case")

5ffdf4b9c257746c16accd716d139aa522f048e2
Luke Hakso <lukehakso@Lukes-MacBook-Air.local>
2023-06-24 11:49:24 -0700

    added 'STATUS' to validation check, changed return to print func

--- validate.py
+++ validate.py
@@ -5,6 +5,7 @@ HEADER_RANGE = 'A11:T11'
 
 def validate_sheet(sheet: Worksheet) -> None:
     pass
+    # Worksheet is passed in as an arg
     # Check if the header row is where we expect it to be
     error_msg = validate_header(sheet,HEADER_RANGE)
     if error_msg is not None:
@@ -29,10 +30,10 @@ def validate_header(sheet: Worksheet, cell_range: str) -> str | None:
 #     get_values returns a list of lists, which is a list of the values in each row. 
     expected_header = sheet.get_values(cell_range)[0]
     missing_column_names = []
-    for expected_column in ['PROJECTED HOURS','SQUIRT BOOM', 'Full Address']:
+    for expected_column in ['STATUS','PROJECTED HOURS','SQUIRT BOOM', 'Full Address']:
         if expected_column not in expected_header:
             print(f"{expected_column} not in the header range")
-            if expected_column.lower() not in [header_val.lower() for header_val in expected_header]:
-                missing_column_names.append(expected_column)
+        if expected_column.lower() not in [header_val.lower() for header_val in expected_header]:
+             missing_column_names.append(expected_column)
     if missing_column_names != []:
-        return f"{missing_column_names} not in the header range, even in constant case"
+        print(f"{missing_column_names} not in the header range, even in constant case")

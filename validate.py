5a845545850d3ca8c5168ad42ec83f611e0c371f
Luke Hakso <lukehakso@Lukes-MacBook-Air.local>
2023-07-07 14:20:50 -0700

    completed validate_col_val and got validate_order to call a variable instead of worksheet every time

--- validate.py
+++ validate.py
@@ -3,26 +3,38 @@ import numpy as np
 from gspread import Worksheet
 
 HEADER_RANGE = "A11:T11"
+END_ROW = "299"
+START_ROW = "14"
+
 class SheetAssumptionViolated(Exception):
     pass
 
 def validate_sheet(sheet: Worksheet) -> None:
     # Worksheet is passed in as an arg
     # Check if the header row is where we expect it to be
-    error_msg1 = validate_header(sheet, HEADER_RANGE) 
-    if error_msg1 is not None:
-       raise SheetAssumptionViolated(error_msg1)
+    header_row = get_header_row(sheet, HEADER_RANGE)
 
-    error_msg2 = validate_order(sheet)
-    if error_msg2 is not None:
-        raise SheetAssumptionViolated(error_msg2)
+    header_error_msg = validate_header(header_row) 
+    if header_error_msg is not None:
+       raise SheetAssumptionViolated(header_error_msg)
+
+    order_error_msg = validate_order(header_row)
+    if order_error_msg is not None:
+        raise SheetAssumptionViolated(order_error_msg)
+    
+    col_val_error_msg = validate_col_val(sheet)
+    if col_val_error_msg is not None:
+        raise SheetAssumptionViolated(col_val_error_msg)
     # Check if the columns we expect to exist are there
 
     # Check if the order is as expected
 
     # Spot check critical columns to see if values are what we think they are
 
-def validate_header(sheet: Worksheet, cell_range: str) -> str | None:
+def get_header_row(sheet:Worksheet, cell_range) -> str:
+    return sheet.get_values(cell_range)[0]
+
+def validate_header(expected_header) -> str | None:
     """
     :param Worksheet: a gspread worksheet object containing one circuit of tasks
     :param cell_range: the A-notation range of where the header should be
@@ -33,7 +45,6 @@ def validate_header(sheet: Worksheet, cell_range: str) -> str | None:
     """
 
     #     get_values returns a list of lists, which is a list of the values in each row.
-    expected_header = sheet.get_values(cell_range)[0]
     missing_column_names = []
     for expected_column in ["STATUS", "PROJECTED HOURS", "SQUIRT BOOM", "Full Address"]:
         if expected_column not in expected_header:
@@ -43,15 +54,43 @@ def validate_header(sheet: Worksheet, cell_range: str) -> str | None:
     if missing_column_names != []:
         return f"{missing_column_names} not in the header range, even in constant case"
     
-def validate_order(sheet: Worksheet):
-   status = sheet.get_values("A11")
-   full_address = sheet.get_values("D11")
-   projected_hours = sheet.get_values("J11")
-   squirt_boom = sheet.get_values("N11")
-   columns = status[0]+full_address[0]+projected_hours[0]+squirt_boom[0]
+def validate_order(header_row) -> str:
+   status = header_row[0]
+   full_address = header_row[3]
+   projected_hours = header_row[9]
+   squirt_boom = header_row[13]
+   columns =[status, full_address, projected_hours, squirt_boom]
    expected_column_names = ["STATUS", "Full Address", "PROJECTED HOURS", "SQUIRT BOOM"]
    if expected_column_names != columns:
        return "Columns out of order"
 
-def validate_col_val(sheet: Worksheet, cell_range: str):
-    pass
+def validate_col_val(sheet: Worksheet)-> str:
+    #spot check critical columns, maybe add full_address later
+
+    status_col_unjoined = sheet.get_values("A"+START_ROW+":""A"+END_ROW)
+    projected_hours_col_unjoined = sheet.get_values("J"+START_ROW+":""J"+END_ROW)
+    status_column = []
+    for item in status_col_unjoined:
+        status_column.append(item[0])
+    projected_hours = []
+    for item in projected_hours_col_unjoined:
+        projected_hours.append(item[0])
+
+    status_bad_cells = []
+    projected_hours_bad_cells = []
+
+    for count, val in enumerate(status_column, 14):
+        if val not in ['Done', 'Not Started', 'In Process']:
+            status_bad_cells.append('A'+str(count))
+
+    for count, val in enumerate(projected_hours, 14):
+        try: 
+            val = float(val)
+            if val>20 or val==0:
+                projected_hours_bad_cells.append('J'+str(count))
+        except ValueError:
+            projected_hours_bad_cells.append('J'+str(count))
+
+    all_bad_cells = [status_bad_cells, projected_hours_bad_cells]
+    if all_bad_cells != [[],[]]:
+        return f"Unexpected values in {all_bad_cells}"
